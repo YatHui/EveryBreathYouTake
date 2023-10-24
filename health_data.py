720e5d923e16e56170c1dc2d53c4b452f9e11461
patricia garcia <patriciagarcia91@hotmail.com>
2023-10-24 12:00:07 +0200

    commited csv file healt_data

---
+++ health_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df_annual_mortality_rate_age65 = pd.read_csv('annual-mortality-rate-from-seasonal-influenza-ages-65.csv')
+df_cancer_death_by_age = pd.read_csv('cancer-deaths-by-age.csv')
+df_pneumonia_respiratory_diseases = pd.read_csv('pneumonia-and-lower-respiratory-diseases-deaths.csv')
+df_public_health_expenditure = pd.read_csv('public-health-expenditure-share-gdp.csv')
+
