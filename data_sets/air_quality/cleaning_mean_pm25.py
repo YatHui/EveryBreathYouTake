612d386f1d1529f0c9471e1f89e27b0b901236eb
Yatwan Hui <yatwan@gmail.com>
2023-10-24 16:46:23 +0200

    Added new column 'Continent'

--- data_sets/air_quality/cleaning_mean_pm25.py
+++ data_sets/air_quality/cleaning_mean_pm25.py
@@ -2,6 +2,7 @@
 # 195 countries from 2010-2019
 
 import pandas as pd
+import country_converter as coco
 
 df = pd.read_csv('data_sets/air_quality/WHO_,annual_mean_pm25.csv')
 
@@ -12,7 +13,7 @@ df_total = df
 df_total = df.iloc[:, [0, 1, 6, 11, 16, 21, 26, 31, 36, 41, 46]]
 
 # Rename column names
-new_columns = ['Countries, territories and aras','2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010' ]
+new_columns = ['Country','2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010' ]
 df_total.columns = new_columns
 
 # Drop top two rows
@@ -24,8 +25,16 @@ columns_to_process = ['2019', '2018', '2017', '2016', '2015', '2014', '2013', '2
 for col in columns_to_process:
     df_total[col] = df_total[col].str.extract(r'^([\d.]+)')
 
+# Use country_converter to assign a Continent to a Country as a new column 
+# pip install country_converter
+
+cc = coco.CountryConverter()
+df_total['continent'] = df_total['Country'].apply(lambda x: cc.convert(x, to='continent'))
+df_total.insert(loc=1, column='Continent', value=df_total['continent'])
+df_total = df_total.drop(columns=['continent'])
+
 # Exporting to clean csv
-df_total.to_csv('./data_sets/air_quality/mean_pm25.csv', index=False)
+df_total.to_csv('data_sets/air_quality/mean_pm25.csv', index=False)
 
 # print(df_total)
 
