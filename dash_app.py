6a7a1998ed5067979f2a699d90039591d974f4f6
dancingalfred <robban021@hotmail.com>
2023-10-26 10:31:14 +0200

    changed minor things with dash_app

--- dash_app.py
+++ dash_app.py
@@ -9,12 +9,15 @@ from dash import Dash, html, dcc, callback, Output, Input
 import os
 
 def init_dash(server):
+    CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+    stylesheet= CURR_DIR_PATH + '\\static\\css\\styles.css'
     dash_app = dash.Dash(
         server=server,
-        routes_pathname_prefix='/dash/'
+        routes_pathname_prefix='/dash/',
+        external_stylesheets=[stylesheet]
     )
     # final data
-    CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+    # CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
     df = pd.read_csv(CURR_DIR_PATH+'.\\health_and_air_final_df.csv')
 
     columns_to_display = df.columns[3:-2].tolist()
@@ -152,3 +155,9 @@ def init_dash(server):
         return create_time_series(dff, yaxis_column_name, axis_type, title)
     
     return dash_app
+
+
+
+def add_header(response):
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    return response
