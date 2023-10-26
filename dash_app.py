1b2637d7647899133065642d1bd1fc6c840225a1
Araz Nabi <araznabi@gmail.com>
2023-10-26 15:55:20 +0200

    tidy things up

--- dash_app.py
+++ dash_app.py
@@ -1,5 +1,6 @@
 # dash_app.py
 import dash
+import plotly.graph_objs as go
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
@@ -22,7 +23,6 @@ def init_dash(server):
     columns_to_display = df.columns[3:-2].tolist()
 
     dash_app.layout = html.Div([
-        html.H1("Health and Air Quality"),
         # Dropdown menu for selecting a country
         dcc.Dropdown(
             id='country-dropdown',
@@ -32,7 +32,6 @@ def init_dash(server):
             'width': '90%',  # Set the width to 80%
             'margin': 'auto',  # Center the dropdown horizontally
             'color': 'blue',  # Set the text color to blue
-            #'background-color': 'lightgray',  # Set the background color to light gray
             '@media screen and (max-width: 600px)': {
             'width': '90%',
         }}), 
@@ -46,46 +45,75 @@ def init_dash(server):
             'width': '90%',  # Set the width to 80%
             'margin': 'auto',  # Center the dropdown horizontally
             'color': 'blue',  # Set the text color to blue
-            #'background-color': 'lightgray',  # Set the background color to light gray
             '@media screen and (max-width: 600px)': {
             'width': '90%',
         }}),
         
         # Plot to show values of the selected column over the years
-        dcc.Graph(id='line-plot'),
+        dcc.Graph(id='line-plot', 
+                style = {
+                    'width': '100%',
+                    'margin': 'auto',
+                    'display': 'inline-block',
+                    '@media screen and (max-width: 600px)': {
+                        'width': '100%',
+                        'margin':1}}),
         
         # Plot to show PM2.5 pollution for the selected country
-        dcc.Graph(id='pm25-plot')
-    ], style = {
-        'width': '80%',
-        'margin': 'auto',
-        'display': 'inline-block',
-        '@media screen and (max-width: 600px)': {
-            'width': '90%',
-        }
-    })
+        dcc.Graph(id='pm25-plot', 
+                style = {
+                    'width': '100%',
+                    'margin': 'auto',
+                    'display': 'inline-block',
+                    '@media screen and (max-width: 600px)': {
+                        'width': '100%',
+                        'margin':1}})
+    ])
 
     # callback to update the line plot
-    @app.callback(
+    @callback(
         Output('line-plot', 'figure'),
         Input('country-dropdown', 'value'),
         Input('column-dropdown', 'value')
     )
     def update_line_plot(selected_country, selected_column):
         filtered_data = df[(df['Location'] == selected_country)]
-        figure = px.line(filtered_data, x='Year', y=selected_column, title=f"{selected_country} - {selected_column}")
+        # Define custom colors for  line
+        custom_color = ['red']
+        figure = px.line(filtered_data, x='Year', 
+                        y=selected_column, 
+                        title=f"{selected_country}<br>{selected_column}",
+                        color_discrete_sequence=custom_color)
         figure.update_yaxes(title_text='per 100.000 people') # Standard value for all graphs
+        figure.update_layout(margin={'l': 40, 'b': 20, 't': 45, 'r': 2})
         return figure
 
     # callback to update the PM2.5 pollution plot based on country
-    @app.callback(
+    @callback(
         Output('pm25-plot', 'figure'),
         Input('country-dropdown', 'value')
     )
     def update_pm25_plot(selected_country):
-        filtered_data = df[(df['Location'] == selected_country)]
-        figure = px.line(filtered_data, x='Year', y='PM 2.5 Airpollution', title=f"{selected_country} - PM2.5 Air Pollution")
-        figure.update_yaxes(title_text='PM 2.5') # Standard value for all graphs
+        filtered_data = df[df['Location'] == selected_country]
+
+        # Create a new DataFrame for each line color based on PM values
+        red_data = filtered_data[filtered_data['PM 2.5 Airpollution'] > 50]
+        orange_data = filtered_data[(filtered_data['PM 2.5 Airpollution'] >= 20) & (filtered_data['PM 2.5 Airpollution'] <= 50)]
+        green_data = filtered_data[(filtered_data['PM 2.5 Airpollution'] < 20)]
+
+        figure = go.Figure()
+
+        if not red_data.empty:
+            figure.add_trace(go.Scatter(x=red_data['Year'], y=red_data['PM 2.5 Airpollution'], name='Red', line=dict(color='red')))
+        if not orange_data.empty:
+            figure.add_trace(go.Scatter(x=orange_data['Year'], y=orange_data['PM 2.5 Airpollution'], name='Orange', line=dict(color='orange')))
+        if not green_data.empty:
+            figure.add_trace(go.Scatter(x=green_data['Year'], y=green_data['PM 2.5 Airpollution'], name='Green', line=dict(color='green')))
+
+        figure.update_layout(
+            title=f"{selected_country}<br>PM2.5 Air Pollution",
+            yaxis_title='PM 2.5 levels',
+            margin={'l': 40, 'b': 20, 't': 45, 'r': 2})
         return figure
     
     return dash_app
