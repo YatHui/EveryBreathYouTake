876530cf6f1a464f9a2daf7fcf55521161300e22
dancingalfred <robban021@hotmail.com>
2023-10-26 20:48:45 +0200

    fixed the third plot

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request, url_for, redirect
 from forms import CustomerEditForm, TransactionForm
 from models import League, db, User, NewsAnnouncements, ChatMessage
 from flask_migrate import Migrate, upgrade
-from dash_app import init_dash,add_header,second_plot
-
+from dash_app import init_dash,add_header,second_plot,third_plot
+#Set up Flask
 app = Flask(__name__)
 app.config.from_object('config.ConfigDebug')
 
@@ -12,10 +12,12 @@ db.app = app
 db.init_app(app)
 migrate = Migrate(app, db)
 
-init_dash(app)  # Initialize the Dash app
-#init_dash_yat(app)
+# Initialize the Dash apps
+init_dash(app)  
 second_plot(app)
+third_plot(app)
 
+#Specify the routes
 @app.route("/")
 def indexPage():
     return render_template('index.html')
--- dash_app.py
+++ dash_app.py
@@ -1,13 +1,15 @@
-# dash_app.py
 import dash
 import plotly.graph_objs as go
-# import dash_core_components as dcc
-# import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 from dash import Dash, html, dcc, callback, Output, Input
 import os
 
+def add_header(response):
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    return response
+
+# - - - - - - - - - - - - - - FIRST PLOT - - - - - - - - - - - - - -  
 def init_dash(server):
     CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
     stylesheet= CURR_DIR_PATH + '\\static\\css\\styles.css'
@@ -118,11 +120,6 @@ def init_dash(server):
     
     return dash_app
 
-
-
-def add_header(response):
-    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
-    return response
 # - - - - - - - - - - - - - - SECOND PLOT - - - - - - - - - - - - - -  
 def second_plot(server):
     df_mean = pd.read_csv("data_sets/air_quality/pm25_mean.csv")  
@@ -230,3 +227,102 @@ def second_plot(server):
 
 
     return app
+
+# - - - - - - - - - - - - - - THIRD PLOT - - - - - - - - - - - - - -  
+
+def third_plot(server):
+
+    # Import data
+    df_mean = pd.read_csv("data_sets/air_quality/pm25_mean.csv")  
+    df_min = pd.read_csv("data_sets/air_quality/pm25_min.csv")  
+    df_max = pd.read_csv("data_sets/air_quality/pm25_max.csv")  
+
+    # Define columns to plot
+    years = df_mean.columns[2:12].tolist()
+    countries = df_mean.iloc[1:192, 0].tolist()
+    continents = df_mean.iloc[1:192, 1].tolist()
+
+    pm_mean = df_mean.iloc[1:192, 2:12].values.tolist()
+    pm_min = df_min.iloc[1:192, 2:12].values.tolist()
+    pm_max = df_max.iloc[1:192, 2:12].values.tolist()
+
+    continent_colour_map = {
+        'Africa': 'brown','America': 'green','Asia': 'red', 'Europe': 'orange','Oceania': 'blue'}
+
+    app = dash.Dash(
+        server=server,
+        routes_pathname_prefix='/dash3/')
+
+
+    fig = go.Figure()
+
+    legend_items = []
+
+    for country_index, country in enumerate(countries):
+        selected_pm_mean = pm_mean[country_index]
+        selected_pm_min = pm_min[country_index]
+        selected_pm_max = pm_max[country_index]
+        selected_continent = continents[country_index]
+
+        # Add traces for Mean, Min, Max
+        
+        # fig.add_trace(go.Scatter(x=years, y=selected_pm_max, 
+        #                         mode='lines', line=dict(dash='solid', width=2), 
+        #                         name=f'{country} Max')),
+        fig.add_trace(go.Scatter(x=years, y=selected_pm_mean, 
+                                mode='lines', line=dict(dash='solid', width=1, color=continent_colour_map[selected_continent]), 
+                                name=f'{selected_continent} Mean'))
+        # fig.add_trace(go.Scatter(x=years, y=selected_pm_min, 
+        #                         mode='lines', line=dict(dash='solid', width=0.5), 
+        #                         name=f'{country} Min', 
+        #                         line=dict(color=continent_colour_map[selected_continent])))
+
+    # Store legend item for this continent
+        legend_items.append(dict(
+        label=selected_continent,
+        method='restyle',
+        args=['visible', [
+            True if trace.name.startswith(selected_continent) 
+            else False for trace in fig.data]]))
+
+    # Set the continent colour
+        #fig.update_traces(line=dict(color=continent_colour_map[selected_continent]))
+
+    # Set layout
+    fig.update_layout(
+        title=f'Fine air particles smaller than 2.5 micrometers (mean values world-wide)',
+        xaxis_title='Year',
+        yaxis_title='PM2.5 (µg/m³)',
+        yaxis_range=[0, 115],
+        updatemenus=[
+            dict(
+                type='buttons',
+                showactive=False,
+                buttons=legend_items
+            )
+        ]
+    )
+
+    app.layout = html.Div([
+        dcc.Graph(figure=fig)
+    ])
+
+    return app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
